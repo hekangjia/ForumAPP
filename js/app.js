94f88fdd5c2dd59ff8b77bad1018fdcae010b2bf
hekangjia <421206766@qq.com>
2015-08-12 20:32:06 +0800

    init project

---
+++ js/app.js
@@ -0,0 +1,131 @@
+/**
+ * 演示程序当前的 “注册/登录” 等操作，是基于 “本地存储” 完成的
+ * 当您要参考这个演示程序进行相关 app 的开发时，
+ * 请注意将相关方法调整成 “基于服务端Service” 的实现。
+ **/
+(function($, owner) {
+	/**
+	 * 用户登录
+	 **/
+	owner.login = function(loginInfo, callback) {
+		callback = callback || $.noop;
+		loginInfo = loginInfo || {};
+		loginInfo.account = loginInfo.account || '';
+		loginInfo.password = loginInfo.password || '';
+		if (loginInfo.account.length < 5) {
+			return callback('账号最短为 5 个字符');
+		}
+		if (loginInfo.password.length < 6) {
+			return callback('密码最短为 6 个字符');
+		}
+		var users = JSON.parse(localStorage.getItem('$users') || '[]');
+		var authed = users.some(function(user) {
+			return loginInfo.account == user.account && loginInfo.password == user.password;
+		});
+		if (authed) {
+			return owner.createState(loginInfo.account, callback);
+		} else {
+			return callback('用户名或密码错误');
+		}
+	};
+
+	owner.createState = function(name, callback) {
+		var state = owner.getState();
+		state.account = name;
+		state.token = "token123456789";
+		owner.setState(state);
+		return callback();
+	};
+
+	/**
+	 * 新用户注册
+	 **/
+	owner.reg = function(regInfo, callback) {
+		callback = callback || $.noop;
+		regInfo = regInfo || {};
+		regInfo.account = regInfo.account || '';
+		regInfo.password = regInfo.password || '';
+		if (regInfo.account.length < 5) {
+			return callback('用户名最短需要 5 个字符');
+		}
+		if (regInfo.password.length < 6) {
+			return callback('密码最短需要 6 个字符');
+		}
+		if (!checkEmail(regInfo.email)) {
+			return callback('邮箱地址不合法');
+		}
+		// 把注册信息存到本地
+		var users = JSON.parse(localStorage.getItem('$users') || '[]');  //获取本地存储中的用户表
+		users.push(regInfo);// 往本地存储的用户表中追加改用户信息
+		localStorage.setItem('$users', JSON.stringify(users));// 把新的‘用户信息表’存入本地存储中，即更新
+		// 也可以把注册信息通过ajax传入到自己的服务器后台
+		mui.ajax('http://121.40.31.223:80/login/',{
+			data:{
+				account:regInfo.account,
+				password:regInfo.password,
+				email:regInfo.email
+			},
+			dataType:'json',
+			type:'POST',
+			timeout:100000,
+			success:function(result){
+				plus.nativeUI.toast("有数据返回");
+			},
+			error:function(){
+				
+			}
+		})
+		return callback();
+	};
+
+	/**
+	 * 获取当前状态
+	 **/
+	owner.getState = function() {
+		var stateText = localStorage.getItem('$state') || "{}";
+		return JSON.parse(stateText);
+	};
+
+	/**
+	 * 设置当前状态
+	 **/
+	owner.setState = function(state) {
+		state = state || {};
+		localStorage.setItem('$state', JSON.stringify(state));
+		//var settings = owner.getSettings();
+		//settings.gestures = '';
+		//owner.setSettings(settings);
+	};
+
+	var checkEmail = function(email) {
+		email = email || '';
+		return (email.length > 3 && email.indexOf('@') > -1);
+	};
+
+	/**
+	 * 找回密码
+	 **/
+	owner.forgetPassword = function(email, callback) {
+		callback = callback || $.noop;
+		if (!checkEmail(email)) {
+			return callback('邮箱地址不合法');
+		}
+		return callback(null, '新的随机密码已经发送到您的邮箱，请查收邮件。');
+	};
+
+	/**
+	 * 获取应用本地配置
+	 **/
+	owner.setSettings = function(settings) {
+		settings = settings || {};
+		localStorage.setItem('$settings', JSON.stringify(settings));
+	}
+
+	/**
+	 * 设置应用本地配置
+	 **/
+	owner.getSettings = function() {
+		var settingsText = localStorage.getItem('$settings') || "{}";
+		return JSON.parse(settingsText);
+	}
+}(mui, window.app = {}));
